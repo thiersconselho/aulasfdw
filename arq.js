4b544201e773af61f6e5ad30312974c3a41896bd
Thiérs HBC <thiersconselho@prof.una.br>
2019-03-14 15:36:16 -0300

    Primeiro Commit

---
+++ arq.js
@@ -0,0 +1,7 @@
+function soma () {     
+	var n1, n2; 
+     n1 = ocument.getElementById("primeiroNumero").value;     
+     n2 = document.getElementById ("segundoNumero").value;
+     n1 = parseInt (n1); 
+     n2 = parseInt (n2);
+     }
